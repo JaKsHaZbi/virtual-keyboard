6b8200541716e77de7ed3750cd1ff10461ec8ba3
JaKsHaZbi <joker_hazbi@mail.ru>
2023-05-02 00:03:52 +0300

    fix: msg 2

--- virtual-keyboard/scripts/render.js
+++ virtual-keyboard/scripts/render.js
@@ -21,7 +21,7 @@ function render() {
   textArea.id = 'input';
   textArea.rows = 4;
   textArea.cols = 50;
-  textArea.innerHTML = 'Если есть возможность проверьте работу ближе к концу ревью, спасибо';
+  textArea.innerHTML = 'Если есть возможность pls проверьте работу ближе к концу ревью, спасибо';
 
   const infoLast = document.createElement('div');
   infoLast.className = 'info-last';
