5fb27f7712fa9f382c49d4a4fd5b9c20bc81624b
JaKsHaZbi <joker_hazbi@mail.ru>
2023-05-03 00:44:43 +0300

    feat: add keys.js base and animation

--- virtual-keyboard/scripts/render.js
+++ virtual-keyboard/scripts/render.js
@@ -1,36 +1,41 @@
+import { keys } from "./keys.js";
+
 function render() {
-  const body = document.querySelector('body');
+  const body = document.querySelector("body");
 
-  const wrapper = document.createElement('div');
-  wrapper.className = 'wrapper';
+  const wrapper = document.createElement("div");
+  wrapper.className = "wrapper";
 
-  const textarea = document.createElement('section');
-  textarea.className = 'textarea container';
+  const textarea = document.createElement("section");
+  textarea.className = "textarea container";
 
-  const infoFirst = document.createElement('div');
-  infoFirst.className = 'info-first';
+  const infoFirst = document.createElement("div");
+  infoFirst.className = "info-first";
 
-  const infoFirstP = document.createElement('p');
-  infoFirstP.innerHTML = 'Начните печатать или выберите область ввода';
+  const infoFirstP = document.createElement("p");
+  infoFirstP.innerHTML = "Начните печатать или выберите область ввода";
 
-  const infoFirstLink = document.createElement('a');
-  infoFirstLink.href = 'https://github.com/JaKsHaZbi/virtual-keyboard/pull/1';
-  infoFirstLink.innerHTML = 'Pull Request Link';
+  const infoFirstLink = document.createElement("a");
+  infoFirstLink.href = "https://github.com/JaKsHaZbi/virtual-keyboard/pull/1";
+  infoFirstLink.innerHTML = "Pull Request Link";
 
-  const textArea = document.createElement('textarea');
-  textArea.id = 'input';
+  const textArea = document.createElement("textarea");
+  textArea.id = "input";
   textArea.rows = 4;
   textArea.cols = 50;
-  textArea.innerHTML = 'Если есть возможность проверьте пожалуйста работу ближе к концу ревью, спасибо';
+  textArea.innerHTML =
+    "Если есть возможность проверьте пожалуйста работу ближе к концу ревью, спасибо \n" +
+    "Ивенты пока не отрабатываются";
 
-  const infoLast = document.createElement('div');
-  infoLast.className = 'info-last';
+  const infoLast = document.createElement("div");
+  infoLast.className = "info-last";
 
-  const infoLastPFist = document.createElement('p');
-  infoLastPFist.innerHTML = 'Клавиатура создана в операционной системе: Windows';
+  const infoLastPFist = document.createElement("p");
+  infoLastPFist.innerHTML =
+    "Клавиатура создана в операционной системе: Windows";
 
-  const infoLastPSecond = document.createElement('p');
-  infoLastPSecond.innerHTML = 'Переключение языка: левыe клавиши ctrl + alt';
+  const infoLastPSecond = document.createElement("p");
+  infoLastPSecond.innerHTML = "Переключение языка: левыe клавиши ctrl + alt";
 
   infoFirst.append(infoFirstP, infoFirstLink);
   infoLast.append(infoLastPFist, infoLastPSecond);
@@ -42,22 +47,35 @@ function render() {
   body.append(wrapper);
 
   // render keyboard section
-  const keyboard = document.createElement('section');
-  keyboard.className = 'keyboard container';
+  const keyboard = document.createElement("section");
+  keyboard.className = "keyboard container";
+
+  // Object.keys(keys).forEach(key => {
+  //   if (keys[key].id === 1) {
+  //     console.log(keys[key]["en"]["caseDown"]);
+  //   }
+  // });
 
   for (let i = 1; i <= 5; i += 1) {
-    const row = document.createElement('div');
+    const row = document.createElement("div");
     row.className = `row-${i}`;
 
     if (i === 1) {
       for (let j = 1; j <= 14; j += 1) {
-        const btn = document.createElement('div');
-        btn.className = 'btn';
+        const btn = document.createElement("div");
+        btn.className = "btn";
+        // Object.keys(keys).forEach(key => {
+        //   if (keys[key].id === j) {
+        //     btn.innerHTML = keys[key]["en"]["caseDown"];
+        //   }
+        // });
+        fill(btn, j);
+        
         if (j === 1) {
-          btn.className = 'btn dark';
+          btn.className = "btn dark";
         }
         if (j === 14) {
-          btn.className = 'btn large dark';
+          btn.className = "btn large dark";
         }
 
         row.append(btn);
@@ -65,14 +83,16 @@ function render() {
     }
 
     if (i === 2) {
-      for (let j = 1; j <= 15; j += 1) {
-        const btn = document.createElement('div');
-        btn.className = 'btn';
-        if (j === 1) {
-          btn.className = 'btn middle dark';
-        }
+      for (let j = 15; j <= 29; j += 1) {
+        const btn = document.createElement("div");
+        btn.className = "btn";
+        fill(btn, j);
+
         if (j === 15) {
-          btn.className = 'btn dark';
+          btn.className = "btn middle dark";
+        }
+        if (j === 29) {
+          btn.className = "btn dark";
         }
 
         row.append(btn);
@@ -80,11 +100,13 @@ function render() {
     }
 
     if (i === 3) {
-      for (let j = 1; j <= 13; j += 1) {
-        const btn = document.createElement('div');
-        btn.className = 'btn';
-        if (j === 1 || j === 13) {
-          btn.className = 'btn large dark';
+      for (let j = 30; j <= 42; j += 1) {
+        const btn = document.createElement("div");
+        btn.className = "btn";
+        fill(btn, j);
+
+        if (j === 30 || j === 42) {
+          btn.className = "btn large dark";
         }
 
         row.append(btn);
@@ -92,14 +114,15 @@ function render() {
     }
 
     if (i === 4) {
-      for (let j = 1; j <= 14; j += 1) {
-        const btn = document.createElement('div');
-        btn.className = 'btn';
-        if (j === 1) {
-          btn.className = 'btn large dark';
+      for (let j = 43; j <= 56; j += 1) {
+        const btn = document.createElement("div");
+        btn.className = "btn";
+        fill(btn, j);
+        if (j === 43) {
+          btn.className = "btn large dark";
         }
-        if (j === 13 || j === 14) {
-          btn.className = 'btn dark';
+        if (j === 55 || j === 56) {
+          btn.className = "btn dark";
         }
 
         row.append(btn);
@@ -107,14 +130,16 @@ function render() {
     }
 
     if (i === 5) {
-      for (let j = 1; j <= 9; j += 1) {
-        const btn = document.createElement('div');
-        btn.className = 'btn dark';
-        if (j === 1 || j === 6) {
-          btn.className = 'btn middle dark';
+      for (let j = 57; j <= 65; j += 1) {
+        const btn = document.createElement("div");
+        btn.className = "btn dark";
+        fill(btn, j);
+
+        if (j === 57 || j === 63) {
+          btn.className = "btn middle dark";
         }
-        if (j === 4) {
-          btn.className = 'btn space';
+        if (j === 60) {
+          btn.className = "btn space";
         }
 
         row.append(btn);
@@ -127,3 +152,12 @@ function render() {
 }
 
 render();
+
+
+function fill(btn, j) {
+  Object.keys(keys).forEach(key => {
+    if (keys[key].id === j) {
+      btn.innerHTML = keys[key]["en"]["caseDown"];
+    }
+  });
+}
